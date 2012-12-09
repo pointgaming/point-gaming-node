123938e29cfafd692882b6136f09646dcb6316bd
Nick Kezhaya <nick@whitepaperclip.com>
2012-12-09 17:43:23 -0600

    Quote replace, and add streams as a resource

--- config/router.js
+++ config/router.js
@@ -1,38 +1,44 @@
 var router = new geddy.RegExpRouter();
 
-router.get('/').to('News.index');
+router.get("/").to("News.index");
 
 // Basic routes
-// router.match('/moving/pictures/:id', 'GET').to('Moving.pictures');
+// router.match("/moving/pictures/:id", "GET").to("Moving.pictures");
 //
-// router.match('/farewells/:farewelltype/kings/:kingid', 'GET').to('Farewells.kings');
+// router.match("/farewells/:farewelltype/kings/:kingid", "GET").to("Farewells.kings");
 //
 // Can also match specific HTTP methods only
-// router.get('/xandadu').to('Xanadu.specialHandler');
-// router.del('/xandadu/:id').to('Xanadu.killItWithFire');
+// router.get("/xandadu").to("Xanadu.specialHandler");
+// router.del("/xandadu/:id").to("Xanadu.killItWithFire");
 //
 // Resource-based routes
-// router.resource('hemispheres');
+// router.resource("hemispheres");
 //
 // Nested Resource-based routes
-// router.resource('hemispheres', function(){
-//   this.resource('countries');
-//   this.get('/print(.:format)').to('Hemispheres.print');
+// router.resource("hemispheres", function(){
+//   this.resource("countries");
+//   this.get("/print(.:format)").to("Hemispheres.print");
 // });
 
-router.get('/login').to('Main.login');
-router.get('/logout').to('Main.logout');
-router.post('/auth/local').to('Auth.local');
-router.get('/auth/twitter').to('Auth.twitter');
-router.get('/auth/twitter/callback').to('Auth.twitterCallback');
-router.get('/auth/facebook').to('Auth.facebook');
-router.get('/auth/facebook/callback').to('Auth.facebookCallback');
-router.get('/auth/yammer').to('Auth.yammer');
-router.get('/auth/yammer/callback').to('Auth.yammerCallback');
-router.resource('users');
-router.match('/news').to({controller: 'News', action: 'index'});
-router.match('/games').to({controller: 'Games', action: 'index'});
-router.match('/bets').to({controller: 'Bets', action: 'index'});
-router.match('/tournaments').to({controller: 'Tournaments', action: 'index'});
-router.match('/streams').to({controller: 'Streams', action: 'index'});
+// Authentication
+
+router.get("/login").to("Main.login");
+router.get("/logout").to("Main.logout");
+router.post("/auth/local").to("Auth.local");
+router.get("/auth/twitter").to("Auth.twitter");
+router.get("/auth/twitter/callback").to("Auth.twitterCallback");
+router.get("/auth/facebook").to("Auth.facebook");
+router.get("/auth/facebook/callback").to("Auth.facebookCallback");
+router.get("/auth/yammer").to("Auth.yammer");
+router.get("/auth/yammer/callback").to("Auth.yammerCallback");
+router.resource("users");
+
+// Sidebar
+
+router.match("/news").to({controller: "News", action: "index"});
+router.match("/games").to({controller: "Games", action: "index"});
+router.match("/bets").to({controller: "Bets", action: "index"});
+router.match("/tournaments").to({controller: "Tournaments", action: "index"});
+router.resource("streams");
+
 exports.router = router;
