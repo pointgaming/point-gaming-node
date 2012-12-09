1533dbeb51b720231e3966fbf67aca2422ee972e
Nick Kezhaya <nick@whitepaperclip.com>
2012-12-08 18:30:23 -0600

    Async before filter on Application for currentUser

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,4 +1,24 @@
 var Application = function () {
+    this._setCurrentUser = function (next) {
+        var userId = this.session.get("userId"),
+            User = geddy.model.User,
+            that = this;
+
+        that.currentUser = null;
+
+        if (userId) {
+            User.first({ id: userId }, function (err, _user) {
+                console.log(this);
+                console.log(err);
+                that.currentUser = _user;
+                next();
+            });
+        } else {
+            next();
+        }
+    };
+
+    this.before(this._setCurrentUser, { async: true });
 };
 
 exports.Application = Application;
--- config/development.js
+++ config/development.js
@@ -1,14 +1,19 @@
 var config = {
     detailedErrors: true,
     debug: true,
-    hostname: 'localhost',
+    hostname: "localhost",
     port: 9999,
     model: {
-        defaultAdapter: 'memory'
+        defaultAdapter: "riak",
+        riak: {
+            protocol: "http",
+            host: "localhost",
+            port: 8098
+        }
     },
     sessions: {
-        store: 'memory',
-        key: 'sid',
+        store: "memory",
+        key: "sid",
         expiry: 14 * 24 * 60 * 60
     }
 };
