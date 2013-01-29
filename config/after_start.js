ce9d1fe2d9139377267ee127f7cab01adbe7c7d4
Chris Bankston <chris@whitepaperclip.com>
2013-01-28 18:29:52 -0600

    Added code to manage server-side socket.io connections

--- config/after_start.js
+++ config/after_start.js
@@ -1,33 +1,15 @@
-var client = require("redis").createClient(),
-    User = geddy.model.User,
-    S = require("string"),
+var User = geddy.model.User,
     async = require('async'),
     MessageQueue = require('../lib/messagequeue'),
-    EventEmitter = require('events').EventEmitter;
+    EventEmitter = require('events').EventEmitter,
+    connection_manager = require('../lib/pg-chat/manager');
 
 geddy.emitter = new EventEmitter();
 
+var socketManager = new connection_manager();
+
 geddy.io.sockets.on("connection", function (socket) {
-    socket.on("message", function (message) {
-        if (!message.sessionID) {
-            return;
-        }
-
-        client.hget(message.sessionID, "userId", function (err, userId) {
-            var id = userId.replace(/"/g, "");
-
-            if (userId && !err) {
-                User.first({ id: id }, function (err, _user) {
-                    if (_user && !err) {
-                        geddy.io.sockets.emit("chat", {
-                            username: _user.username,
-                            text: S(message.text).escapeHTML().s
-                        });
-                    }
-                });
-            }
-        });
-    });
+  socketManager.handleNewConnection(socket);
 });
 
 var _notifyUserFriends = function(action){
