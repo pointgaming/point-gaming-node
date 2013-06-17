81283407923a9a107d40b4020d1a1debe5b70d92
Chris Bankston <chris@whitepaperclip.com>
2013-06-17 05:00:32 -0500

    Improved Chatroom.Member.getList response data for all chatroom types

--- config/initializers/api_client.js
+++ config/initializers/api_client.js
@@ -1,10 +1,8 @@
 var PointGaming = global.PointGaming,
-    currentUserApiClient = require('../../lib/pg-chat/api/current_user');
+    currentUserApiClient = require('../../lib/pg-chat/api/current_user'),
     streamApiClient = require('../../lib/pg-chat/api/stream'),
-    disputeApiClient = require('../../lib/pg-chat/api/dispute'),
-    userApiClient = require('../../lib/pg-chat/api/user');
+    disputeApiClient = require('../../lib/pg-chat/api/dispute');
 
 PointGaming.current_user_api_client = new currentUserApiClient(PointGaming.config.api_url || 'https://dev.pointgaming.com/');
 PointGaming.stream_api_client = new streamApiClient(PointGaming.config.api_url || 'https://dev.pointgaming.com/', PointGaming.config.api_token);
 PointGaming.dispute_api_client = new disputeApiClient(PointGaming.config.api_url || 'https://dev.pointgaming.com/', PointGaming.config.api_token);
-PointGaming.user_api_client = new userApiClient(PointGaming.config.api_url || 'https://dev.pointgaming.com/', PointGaming.config.api_token);
